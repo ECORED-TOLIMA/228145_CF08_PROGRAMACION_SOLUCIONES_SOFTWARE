7bcaaf191c122f40fea267e5b5b883bbbb966ef3
cveimarsena <vcelis@sena.edu.co>
2026-05-29 07:39:51 -0500

    Video

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -393,11 +393,8 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-          figcaption Video: Leyenda del video
-    
-        
-
+            iframe(width="1401" height="788" src="https://www.youtube.com/embed/FtDLQx0adtA" title="Buenas y malas prácticas en programación" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
+            
 </template>
 
 <script>
